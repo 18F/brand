063ed22a2d4863753dc716da8d631f6161ab0867
Shawn Allen <shawn.allen@gsa.gov>
2017-01-18 16:07:07 -0800

    set baseurl for 18f-pages-staging, too

--- _plugins/18f-pages.rb
+++ _plugins/18f-pages.rb
@@ -1,4 +1,6 @@
-if ENV['BRANCH'] == '18f-pages'
+PAGES_BRANCHES = ['18f-pages', '18f-pages-staging']
+
+if PAGES_BRANCHES.include? ENV['BRANCH']
     Jekyll::Hooks.register :site, :pre_render do |site|
         site.config['baseurl'] = '/brand'
     end
